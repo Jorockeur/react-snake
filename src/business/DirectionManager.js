f299d5ebd168324a08d40128af33b4ec1db67d85
jorockeur <lacassagnev@gmail.com>
2019-03-21 18:51:57 +0100

    fix multiple direction instructions in the same interval

--- src/business/DirectionManager.js
+++ src/business/DirectionManager.js
@@ -2,6 +2,7 @@ import { computed, observable } from "mobx";
 
 class DirectionManager{
     @observable _direction;
+    @observable _blockDirection = false;
 
     constructor() {
         this.reset();
@@ -9,19 +10,24 @@ class DirectionManager{
     }
 
     startListening() {
-        let monHandler = this.captureKeyboard.bind(this); //bind le contexte courant
-        window.addEventListener('keydown', monHandler, true);
+        let handler = this.captureKeyboard.bind(this); //bind le contexte courant
+        window.addEventListener('keydown', handler, true);
     }
 
     captureKeyboard(e) {
+        if (this._blockDirection) return;
         if (e.key === 'ArrowUp' && this._direction !== 'down') {
             this._direction = 'up';
+            this._blockDirection = true;
         } else if (e.key === 'ArrowDown' && this._direction !== 'up') {
             this._direction = 'down';
+            this._blockDirection = true;
         } else if (e.key === 'ArrowLeft' && this._direction !== 'right') {
             this._direction = 'left';
+            this._blockDirection = true;
         } else if (e.key === 'ArrowRight' && this._direction !== 'left') {
             this._direction = 'right';
+            this._blockDirection = true;
         }
         e.preventDefault();
     }
