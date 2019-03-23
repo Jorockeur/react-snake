363d8dd7125ce4a59dd0b87744b7bd9c3dd24b6f
jorockeur <lacassagnev@gmail.com>
2019-03-23 17:12:46 +0100

    rainbow text when pride is on, shake every 8s to show user interactivity

--- src/components/Pride/Pride.js
+++ src/components/Pride/Pride.js
@@ -10,22 +10,81 @@ import { inject, observer } from "mobx-react";
         super(props);
         this.state = {
             isPrideOn: false,
+            buttonStyle: '',
+            spanStyle: '',
+            isPrideTried: false,
         }
+    }
+
+    componentDidMount() {
+        setInterval(() => this.handleShakeInterval(), 8000);
+    }
+
+    handleShakeInterval() {
+        if (this.state.isPrideTried) return;
+        const prideButton = document.getElementsByClassName('prideButton')[0];
+        prideButton.classList.add('tryMe');
+        setTimeout(() => prideButton.classList.remove('tryMe'), 820);
     }
 
     handleClick() {
         this.props.setStateInParent({ isPrideOn: !this.state.isPrideOn });
-        this.setState({ isPrideOn: !this.state.isPrideOn })
+        this.setState({ isPrideOn: !this.state.isPrideOn, isPrideTried: true });
+    }
+
+    handleMouseOver() {
+        this.setState({ buttonStyle: 'prideVibes', spanStyle: 'pride' });
+    }
+
+    handleMouseOut() {
+        this.setState({ buttonStyle: '', spanStyle: '' });
     }
 
     render() {
         return (
             <React.Fragment>
-                { this.props.nightmode ? (
-                    <button onClick={ () => this.handleClick() } style={{ backgroundColor: '#ffffff', color: '#333333' }}>Show your pride!</button>
-                ) : (
-                    <button onClick={ () => this.handleClick() }>Show your pride!</button>
-                )}
+                {
+                    (() => {
+                        if (this.props.nightmode && this.state.isPrideOn)
+                            return (
+                                <button
+                                    onClick={ () => this.handleClick() }
+                                    onMouseOver={ () => this.handleMouseOver() }
+                                    onMouseOut={ () => this.handleMouseOut() }
+                                    className={ `prideButton ${ this.state.buttonStyle }` }
+                                    style={{ backgroundColor: '#ffffff', color: '#333333' }}
+                                ><span className={ `pride ${this.state.spanStyle}` }>Show your pride!</span></button>
+                            );
+                        if (this.props.nightmode && !this.state.isPrideOn)
+                            return (
+                                <button
+                                    onClick={ () => this.handleClick() }
+                                    onMouseOver={ () => this.handleMouseOver() }
+                                    onMouseOut={ () => this.handleMouseOut() }
+                                    className={ `prideButton ${ this.state.buttonStyle }` }
+                                    style={{ backgroundColor: '#ffffff', color: '#333333' }}
+                                ><span className={ this.state.spanStyle }>Show your pride!</span></button>
+                            );
+                        if (!this.props.nightmode && this.state.isPrideOn)
+                            return (
+                                <button
+                                    onClick={ () => this.handleClick() }
+                                    onMouseOver={ () => this.handleMouseOver() }
+                                    onMouseOut={ () => this.handleMouseOut() }
+                                    className={ `prideButton ${ this.state.buttonStyle }` }
+                                ><span className={ `pride ${this.state.spanStyle}` }>Show your pride!</span></button>
+                            );
+                        else
+                            return (
+                                <button
+                                    onClick={ () => this.handleClick() }
+                                    onMouseOver={ () => this.handleMouseOver() }
+                                    onMouseOut={ () => this.handleMouseOut() }
+                                    className={ `prideButton ${ this.state.buttonStyle }` }
+                                ><span className={ this.state.spanStyle }>Show your pride!</span></button>
+                            );
+                    })()
+                }
             </React.Fragment>
         )
     };
